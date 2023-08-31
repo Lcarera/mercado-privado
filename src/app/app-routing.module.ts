56d0ba4c736c24d781008bb81b5569cda647f8c5
lcarera <lcarera04@gmail.com>
2023-08-30 22:45:12 -0300

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ItemsComponent } from './items/items.component';
+import { ItemListComponent } from './item-list/item-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'items', component: ItemListComponent , pathMatch: 'full'},
+  { path: 'items/:search', component: ItemListComponent , pathMatch: 'full'},
+  { path: 'items/:id', component: ItemsComponent , pathMatch: 'full'},
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '**', redirectTo: '/', pathMatch: 'full'}
+  // Other routes...
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
