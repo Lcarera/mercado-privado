0eabcc03318a9207f1e45e6406fe0a55668e5ff8
lcarera <lcarera04@gmail.com>
2023-09-03 23:25:56 -0300

    added item data fetch

--- src/app/item.service.ts
+++ src/app/item.service.ts
@@ -1,20 +1,29 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpRequest } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ItemService {
-
   private apiUrl = 'https://api.mercadolibre.com/';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getItems(searchText: string) {
-    return this.http.get<any[]>(`${this.apiUrl}sites/MLA/search?q=${searchText}&limit=5`);
+    return this.http.get<any[]>(
+      `${this.apiUrl}sites/MLA/search?q=${searchText}&limit=5`
+    );
   }
 
   getCategories(categoryId: string) {
     return this.http.get<any[]>(`${this.apiUrl}categories/${categoryId}`);
+  }
+
+  getItem(itemId: string) {
+    return this.http.get<any[]>(`${this.apiUrl}items/${itemId}`);
+  }
+
+  getItemDescription(itemId: string) {
+    return this.http.get<any[]>(`${this.apiUrl}items/${itemId}/description`);
   }
 }
